d0984f0b03510464613908bdc5cc2f013e6d24dc
ipeters123 <isaiah.peters64@gmail.com>
2023-03-14 15:46:56 +0000

    comments

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const suits = ["♦", "♥", "♠", "♣"];
 const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 var suit = suits[Math.floor(Math.random() * 3)];
 var value = values[Math.floor(Math.random() * 12)];
+// setting the onload for
 window.onload = function() {
   if (suit == "♥" || suit == "♦") {
     document.querySelector(".upperSuit").style.color = "red";
