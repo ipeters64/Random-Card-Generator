ebea5950b603718b58434a614ac1cd1c16362034
ipeters123 <isaiah.peters64@gmail.com>
2023-06-26 03:09:06 +0000

    comments

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+// defining the variables suits and values to appear when clicked again //
 const suits = ["♦", "♥", "♠", "♣"];
 const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 var suit = suits[Math.floor(Math.random() * 3)];
